80fbeb2efb9cc3a8c50c71815e2312c429f31747
Anuj Choudhary <anujmehlawat9@gmail.com>
2025-12-02 19:39:25 +0530

    Delete userRoutes.js
    Duplicate

--- userRoutes.js
+++
@@ -1,9 +0,0 @@
-import Router from "express";
-import { signup, loginUser } from "../controllers/auth.js";
-
-const router = Router();
-
-router.post("/register", signup);
-router.post("/login", loginUser);
-
-export default router;
